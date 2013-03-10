a21ef6edd745f0ae541f34e1f85096137bad35ec
Yunpeng Zhou <argoran@gmail.com>
2013-03-10 16:49:49 +0100

    显示 抛射类物品造成的伤害(弓，火球)，显示更多伤害相关信息

--- src/com/gmail/argoran/helloWorldPlugin/damageIndicator/DamageListener.java
+++ src/com/gmail/argoran/helloWorldPlugin/damageIndicator/DamageListener.java
@@ -1,7 +1,9 @@
 package com.gmail.argoran.helloWorldPlugin.damageIndicator;
 
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
@@ -25,14 +27,30 @@ public class DamageListener implements Listener {
 			EntityDamageByEntityEvent edbeEvent = (EntityDamageByEntityEvent) _event;
 			Entity srcEntity = edbeEvent.getDamager();
 			if (srcEntity instanceof Player) {
-				if (entity instanceof Player) {
-					Player srcPlayer = (Player) srcEntity;
+				Player srcPlayer = (Player) srcEntity;
+				String weaponName = srcPlayer.getItemInHand().getType().toString();
+				if (entity instanceof Player) {					
 					Player rcvPlayer = (Player) entity;
-					this.plugin_.getServer().broadcastMessage(MessageColor.colorPlayerName(srcPlayer.getDisplayName()) + " has dealt " + MessageColor.colorDamage(_event.getDamage()) + " damage(s) to " + MessageColor.colorPlayerName(rcvPlayer.getDisplayName()) + " .");
+					this.plugin_.getServer().broadcastMessage(MessageColor.colorPlayerName(srcPlayer.getDisplayName()) + " has dealt " + MessageColor.colorDamage(_event.getDamage()) + " damage(s) to " + MessageColor.colorPlayerName(rcvPlayer.getDisplayName()) + " [" + weaponName + "] .");
 				} else {
-					Player player = (Player) srcEntity;
-					this.plugin_.getServer().broadcastMessage(MessageColor.colorPlayerName(player.getDisplayName()) + " has dealt " + MessageColor.colorDamage(_event.getDamage()) + " damage(s) to " + _event.getEntityType().getName() + " .");
+					this.plugin_.getServer().broadcastMessage(MessageColor.colorPlayerName(srcPlayer.getDisplayName()) + " has dealt " + MessageColor.colorDamage(_event.getDamage()) + " damage(s) to " + _event.getEntityType().getName() + " [" + weaponName + "] .");
 				}					
+			} else if (srcEntity instanceof Projectile) {
+				Projectile proj = (Projectile) srcEntity;
+				LivingEntity shooter = proj.getShooter();
+				if (shooter instanceof Player) {
+					Player srcPlayer = (Player) shooter;
+					String weaponName = srcPlayer.getItemInHand().getType().toString();
+					if (entity instanceof Player) {					
+						Player rcvPlayer = (Player) entity;
+						this.plugin_.getServer().broadcastMessage(MessageColor.colorPlayerName(srcPlayer.getDisplayName()) + " has dealt " + MessageColor.colorDamage(_event.getDamage()) + " damage(s) to " + MessageColor.colorPlayerName(rcvPlayer.getDisplayName()) + " [" + weaponName + "] .");
+					} else {
+						this.plugin_.getServer().broadcastMessage(MessageColor.colorPlayerName(srcPlayer.getDisplayName()) + " has dealt " + MessageColor.colorDamage(_event.getDamage()) + " damage(s) to " + _event.getEntityType().getName() + " [" + weaponName + "] .");
+					}
+				} else if (entity instanceof Player) {
+					Player player = (Player) entity;
+					this.plugin_.getServer().broadcastMessage(MessageColor.colorPlayerName(player.getDisplayName()) + " has taken " + MessageColor.colorDamage(_event.getDamage()) + " damage(s) [" + shooter.getType().getName() + " -> " + srcEntity.getType().getName() + "] .");
+				}
 			} else if (entity instanceof Player) {
 				Player player = (Player) entity;
 				this.plugin_.getServer().broadcastMessage(MessageColor.colorPlayerName(player.getDisplayName()) + " has taken " + MessageColor.colorDamage(_event.getDamage()) + " damage(s) [" + srcEntity.getType().getName() + "] .");
